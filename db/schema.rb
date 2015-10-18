9590fcc808a08c41ec56dcc826ae3b255bbb85ae
Judy Long <long.judy92@gmail.com>
2015-10-18 19:03:21 -0400

    schema after versions migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017214352) do
+ActiveRecord::Schema.define(version: 20151018211650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20151017214352) do
     t.string   "property"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.integer  "card_id",    null: false
+    t.string   "set_name",   null: false
+    t.string   "print_tag",  null: false
+    t.string   "rarity",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
