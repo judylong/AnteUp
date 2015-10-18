9651320a8dbd8647cf13aa6141f59b41ec7c3055
Judy Long <long.judy92@gmail.com>
2015-10-18 19:04:36 -0400

    generate versions controller

---
+++ app/controllers/versions_controller.rb
@@ -0,0 +1,16 @@
+class VersionsController < ApplicationController
+  def create
+    card = Card.where({id: version_params['card_id']})
+    @version = card.version.new(version_params)
+    if version.save
+      render json: "success"
+    else
+      render json: @version.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def version_params
+    params.require(:version).permit(:card_id, :set_name, :print_tag, :rarity)
+  end
+end
