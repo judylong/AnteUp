c41c5a23be0da96852fa553bb4abf0eb1ef57274
Judy Long <long.judy92@gmail.com>
2015-10-18 19:03:53 -0400

    add associations between cards and versions

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,2 +1,7 @@
 class Version < ActiveRecord::Base
+  validates :print_tag, uniqueness: true
+  validates :card_id, uniqueness: {scope: :set_name}
+  validates :card_id, uniqueness: {scope: :print_tag}
+
+  belongs_to :card
 end
