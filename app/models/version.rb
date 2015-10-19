cfd6d1f63b28e400900f745627b67ecc48686edc
Judy Long <long.judy92@gmail.com>
2015-10-18 20:53:35 -0400

    fix version validations

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,6 @@
 class Version < ActiveRecord::Base
   validates :print_tag, uniqueness: true
-  validates :card_id, uniqueness: {scope: :set_name}
-  validates :card_id, uniqueness: {scope: :print_tag}
+  validates :card_id, uniqueness: {scope: [:set_name, :print_tag]}
   validates :card_id, :set_name, :print_tag, :rarity, presence: true
 
   belongs_to :card
