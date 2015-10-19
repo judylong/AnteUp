fe4d287f58918af7895b40e303ef7fb63a3ab99b
Judy Long <long.judy92@gmail.com>
2015-10-18 22:01:32 -0400

    make card factory and make spec for card model

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :card do
-    
+    name { Faker::Name.name}
+    card_text { Faker::Lorem.sentence }
+    card_type { Faker::Team.creature }
   end
 
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model methods" do
+
+  end
+
+  describe "validations" do
+    subject { FactoryGirl.build(:card) }
+
+    it { should validate_uniqueness_of(:name) }
+
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:card_text)}
+    it { should validate_presence_of(:card_type)}
+  end
+
+  describe "associations" do
+    it { should have_many(:versions)}
+  end
+
+  describe "error messages" do
+    it "fails validation with a non-unique name expecting a specific message" do
+      non_unique_card = Card.new({name: "Dark Magician"})
+      non_unique_card.valid?
+      expect(non_unique_card.errors[:name]).to include("has already been taken")
+    end
+
+    it "fails validation with no name expecting a specific message" do
+      no_name_card = Card.new()
+      no_name_card.valid?
+      expect(no_name_card.errors[:name]).to include("can't be blank")
+    end
+
+    it "fails validation with no card_text expecting a specific message" do
+      no_card_text_card = Card.new()
+      no_card_text_card.valid?
+      expect(no_card_text_card.errors[:card_text]).to include("can't be blank")
+    end
+
+    it "fails validation with no card_type expecting a specific message" do
+      no_card_type_card = Card.new()
+      no_card_type_card.valid?
+      expect(no_card_type_card.errors[:card_type]).to include("can't be blank")
+    end
+  end
+
 end
