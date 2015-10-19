3fdb271fff60112cc8268161156ac95896e58aa5
Judy Long <long.judy92@gmail.com>
2015-10-18 23:31:51 -0400

    fix uniqueness scope

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,6 +1,6 @@
 class Version < ActiveRecord::Base
-  validates :print_tag, uniqueness: true
-  validates :card_id, uniqueness: {scope: [:set_name, :print_tag]}
+  validates :print_tag, uniqueness: { scope: [:rarity, :set_name] }
+  validates :card_id, uniqueness: { scope: [:set_name, :print_tag, :rarity] }
   validates :card_id, :set_name, :print_tag, :rarity, presence: true
 
   belongs_to :card
