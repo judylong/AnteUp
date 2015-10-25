f94611180d07cdffbf4dce8b0d84b9a80bcdef45
Judy Long <long.judy92@gmail.com>
2015-10-25 18:16:14 -0400

    set up react router

---
+++ app/assets/javascripts/ante_up.js.jsx
@@ -0,0 +1,11 @@
+$(function() {
+  var Router = ReactRouter.Router;
+  var Route = ReactRouter.Route;
+
+  var rootEl = document.getElementById("content");
+
+  React.render((
+    <Router>
+    </Router>
+  ), rootEl)
+});
