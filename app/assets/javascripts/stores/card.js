d9504a3ecfe2e06e4c7e83863d02c7c5e458209d
Judy Long <long.judy92@gmail.com>
2015-10-25 22:43:48 -0400

    update card store for card show component

--- app/assets/javascripts/stores/card.js
+++ app/assets/javascripts/stores/card.js
@@ -1,6 +1,7 @@
 (function(rootEl) {
   var _cards = [];
   var CARDS_INDEX_CHANGE_EVENT = "cardsIndexChange";
+  var CARD_CHANGE_EVENT = "cardChange"
 
   var resetCards = function(cards) {
     _cards = cards.slice(0);
@@ -11,6 +12,15 @@
       return _cards.slice(0);
     },
 
+    find: function(id) {
+      var card;
+      _cards.forEach(function(c) {
+        if (c.id === id) { card = c; }
+      })
+
+      return card;
+    },
+
     addCardsIndexChangeListener: function(callback) {
       this.on(CARDS_INDEX_CHANGE_EVENT, callback);
     },
@@ -19,12 +29,23 @@
       this.removeListener(CARDS_INDEX_CHANGE_EVENT, callback);
     },
 
+    addCardChangeListener: function(callback) {
+      this.on(CARD_CHANGE_EVENT, callback);
+    },
+
+    removeCardChangeListener: function(callback) {
+      this.removeListener(CARD_CHANGE_EVENT, callback)
+    },
+
     dispatcherID: AppDispatcher.register(function(payload) {
       switch (payload.actionType) {
         case AnteUpConstants.CARDS_RECEIVED:
           resetCards(payload.cards);
           CardStore.emit(CARDS_INDEX_CHANGE_EVENT);
+          CardStore.emit(CARD_CHANGE_EVENT);
           break;
+        case AnteUpConstants.CARD_RECEIVED:
+          CardStore.emit(CARD_CHANGE_EVENT);
         default:
 
       }
