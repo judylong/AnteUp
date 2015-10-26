f156b0392d0c1ba1a8fc8941fe3f4e904501af2b
Judy Long <long.judy92@gmail.com>
2015-10-25 20:51:40 -0400

    set up routes

--- app/assets/javascripts/ante_up.js.jsx
+++ app/assets/javascripts/ante_up.js.jsx
@@ -3,9 +3,23 @@ $(function() {
   var Route = ReactRouter.Route;
 
   var rootEl = document.getElementById("content");
+  var App = React.createClass({
+    render: function() {
+      return (
+        <div>
+          <header><h1>Ante Up</h1></header>
+          {this.props.children}
+        </div>
+      );
+    }
+  });
 
-  React.render((
-    <Router>
-    </Router>
-  ), rootEl)
+  var routes = (
+    <Route path="/" component={App}>
+      <Route path="cards" component={CardsIndex}/>
+      <Route path="cards/:cardId" component={CardShow}/>
+    </Route>
+  );
+
+  React.render(<Router>{routes}</Router>, rootEl);
 });
